d53ae43030a8b75fd3e5ffc6245899ef39706ae2
Bioded-001 <mat.fox2002@gmail.com>
2023-06-25 07:41:59 +0800

    up

--- src/main/java/com/mycompany/java/gui/visualnoteapp/VisualNoteApp.java
+++ src/main/java/com/mycompany/java/gui/visualnoteapp/VisualNoteApp.java
@@ -13,6 +13,7 @@ package com.mycompany.java.gui.visualnoteapp;
 //
 //
 
+// import necessary packages and classes
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.io.File;
@@ -40,7 +41,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 
-class VisualNote extends JFrame{
+// the VisualNote class extends JFrame, indicating it's a GUI window
+class VisualNote extends JFrame {
+    // declare all necessary components
     private JTextArea noteTextArea;    
     private JMenuBar menuBar;
     private JMenu fileMenu;
@@ -62,151 +65,167 @@ class VisualNote extends JFrame{
     private JMenuItem pasteMenuItem;
     private JPanel panel;
     private File currentFile;
+    // constructor for VisualNote
     public VisualNote() {
-        setTitle("VisualNote");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1200, 800);
-        
-        noteTextArea = new JTextArea();
-        noteTextArea.setText(new String("".getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8));
-        scrollPane = new JScrollPane(noteTextArea);
-        
-        menuBar = new JMenuBar();
-        fileMenu = new JMenu("File");
-        newMenuItem = new JMenuItem("New");
-        openMenuItem = new JMenuItem("Open");
-        saveMenuItem = new JMenuItem("Save");
-        saveAsMenuItem = new JMenuItem("Save As");
-        exitMenuItem = new JMenuItem("Exit");
-        
-        fileMenu.add(newMenuItem);
-        fileMenu.add(openMenuItem);
-        fileMenu.add(saveMenuItem);
-        fileMenu.add(saveAsMenuItem);
-        fileMenu.addSeparator();
-        fileMenu.add(exitMenuItem);
-        menuBar.add(fileMenu);
-        
-        toolBar = new JToolBar();
-        
-        newButton = new JButton(resizeIcon("image/newfileicon.png", 20, 20));
-        openButton = new JButton(resizeIcon("image/openfileicon.png",20, 20));
-        saveButton = new JButton(resizeIcon("image/savefileicon.png",20,20));
-        subButton = new JButton(resizeIcon("image/icons8-subtract-button-50.png",20,20));
-        addButton = new JButton(resizeIcon("image/icons8-add-button-50.png",20,20));
+    // set window properties
+    // set up components, layout, and event handlers
+        setTitle("VisualNote");  // Sets the title of the JFrame to "VisualNote"
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Sets the default close operation for the JFrame (the application will exit when the JFrame is closed)
+        setSize(1200, 800);  // Sets the size of the JFrame to 1200 pixels wide and 800 pixels tall
 
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.add(newButton);
-        panel.add(openButton);
-        panel.add(saveButton);
-        panel.add(subButton);
-        toolBar.add(panel);
- 
-        popupMenu = new JPopupMenu();
-        cutMenuItem = new JMenuItem("Cut");
-        copyMenuItem = new JMenuItem("Copy");
-        pasteMenuItem = new JMenuItem("Paste");
-        popupMenu.add(cutMenuItem);
-        popupMenu.add(copyMenuItem);
-        popupMenu.add(pasteMenuItem);
-        
-        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        southPanel.add(addButton);
-        
-        noteTextArea.setComponentPopupMenu(popupMenu);        
-        
-        setJMenuBar(menuBar);
-        getContentPane().add(toolBar, BorderLayout.PAGE_START);
-        getContentPane().add(scrollPane, BorderLayout.CENTER);
-        getContentPane().add(southPanel, BorderLayout.SOUTH);
-        
-        subButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                boolean check = promptSaveBeforeExit();
-                if(check){
-                    dispose();
+        // JTextArea for writing and editing notes
+        noteTextArea = new JTextArea();  // Initializes a new JTextArea for writing and editing notes
+        noteTextArea.setText(new String("".getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8));  // Sets the initial text in the JTextArea to an empty string
+        scrollPane = new JScrollPane(noteTextArea);  // Adds the JTextArea to a JScrollPane to allow for scrolling when the text exceeds the size of the JTextArea
+
+        // Menu bar and file menu initialization
+        menuBar = new JMenuBar();  // Initializes a new JMenuBar
+        fileMenu = new JMenu("File");  // Initializes a new JMenu with the label "File"
+        newMenuItem = new JMenuItem("New");  // Initializes a new JMenuItem with the label "New"
+        openMenuItem = new JMenuItem("Open");  // Initializes a new JMenuItem with the label "Open"
+        saveMenuItem = new JMenuItem("Save");  // Initializes a new JMenuItem with the label "Save"
+        saveAsMenuItem = new JMenuItem("Save As");  // Initializes a new JMenuItem with the label "Save As"
+        exitMenuItem = new JMenuItem("Exit");  // Initializes a new JMenuItem with the label "Exit"
+
+        // Adding menu items to the file menu and the file menu to the menu bar
+        fileMenu.add(newMenuItem);  // Adds the "New" JMenuItem to the "File" JMenu
+        fileMenu.add(openMenuItem);  // Adds the "Open" JMenuItem to the "File" JMenu
+        fileMenu.add(saveMenuItem);  // Adds the "Save" JMenuItem to the "File" JMenu
+        fileMenu.add(saveAsMenuItem);  // Adds the "Save As" JMenuItem to the "File" JMenu
+        fileMenu.addSeparator();  // Adds a separator line to the "File" JMenu
+        fileMenu.add(exitMenuItem);  // Adds the "Exit" JMenuItem to the "File" JMenu
+        menuBar.add(fileMenu);  // Adds the "File" JMenu to the JMenuBar
+
+        // Tool bar and button initialization
+        toolBar = new JToolBar();  // Initializes a new JToolBar
+        newButton = new JButton(resizeIcon("image/newfileicon.png", 20, 20));  // Initializes a new JButton with an icon from the specified path
+        openButton = new JButton(resizeIcon("image/openfileicon.png",20, 20));  // Initializes a new JButton with an icon from the specified path
+        saveButton = new JButton(resizeIcon("image/savefileicon.png",20,20));  // Initializes a new JButton with an icon from the specified path
+        subButton = new JButton(resizeIcon("image/icons8-subtract-button-50.png",20,20));  // Initializes a new JButton with an icon from the specified path
+        addButton = new JButton(resizeIcon("image/icons8-add-button-50.png",20,20));  // Initializes a new JButton with an icon from the specified path
+
+        // Adding buttons to the tool bar
+        panel = new JPanel();  // Initializes a new JPanel
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));  // Sets the layout manager for the JPanel to BoxLayout, which arranges the components along the x-axis
+        panel.add(newButton);  // Adds the "New" JButton to the JPanel
+        panel.add(openButton);  // Adds the "Open" JButton to the JPanel
+        panel.add(saveButton);  // Adds the "Save" JButton to the JPanel
+        panel.add(subButton);  // Adds the "Subtract" JButton to the JPanel
+        toolBar.add(panel);  // Adds the JPanel to the JToolBar
+
+        // Popup menu for text editing
+        popupMenu = new JPopupMenu();  // Initializes a new JPopupMenu
+        cutMenuItem = new JMenuItem("Cut");  // Initializes a new JMenuItem with the label "Cut"
+        copyMenuItem = new JMenuItem("Copy");  // Initializes a new JMenuItem with the label "Copy"
+        pasteMenuItem = new JMenuItem("Paste");  // Initializes a new JMenuItem with the label "Paste"
+        popupMenu.add(cutMenuItem);  // Adds the "Cut" JMenuItem to the JPopupMenu
+        popupMenu.add(copyMenuItem);  // Adds the "Copy" JMenuItem to the JPopupMenu
+        popupMenu.add(pasteMenuItem);  // Adds the "Paste" JMenuItem to the JPopupMenu
+
+        // Adds the addButton to the southPanel and sets the popupMenu as the context menu for the noteTextArea
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));  // Initializes a new JPanel with a FlowLayout that aligns its components to the right
+        southPanel.add(addButton);  // Adds the "Add" JButton to the southPanel
+        noteTextArea.setComponentPopupMenu(popupMenu);  // Sets the JPopupMenu as the context menu for the JTextArea
+
+        // Adding the menu bar, tool bar, JTextArea in JScrollPane, and southPanel to the JFrame
+        setJMenuBar(menuBar);  // Adds the JMenuBar to the JFrame
+        getContentPane().add(toolBar, BorderLayout.PAGE_START);  // Adds the JToolBar to the top (PAGE_START) of the JFrame
+        getContentPane().add(scrollPane, BorderLayout.CENTER);  // Adds the JScrollPane (which contains the JTextArea) to the center of the JFrame
+        getContentPane().add(southPanel, BorderLayout.SOUTH);  // Adds the southPanel (which contains the "Add" JButton) to the bottom (SOUTH) of the JFrame
+
+        // Action listeners for the buttons and menu items
+        subButton.addActionListener(new ActionListener() {  // Adds an action listener to the "Subtract" JButton
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Subtract" JButton is clicked
+                boolean check = promptSaveBeforeExit();  // Calls the method promptSaveBeforeExit() and stores its return value in the variable check
+                if(check){  // If check is true...
+                    dispose();  // ...dispose of the JFrame
                 }
             }
         });
         
-        addButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                new VisualNote();
+        addButton.addActionListener(new ActionListener() {  // Adds an action listener to the "Add" JButton
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Add" JButton is clicked
+                new VisualNote();  // Creates a new instance of the VisualNote class
             }
         });
 
-        newMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                createNewNote();
+        newMenuItem.addActionListener(new ActionListener() {  // Adds an action listener to the "New" JMenuItem
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "New" JMenuItem is selected
+                createNewNote();  // Calls the method createNewNote()
             }
         });
-        
-        openMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                openNote();
+
+        openMenuItem.addActionListener(new ActionListener() {  // Adds an action listener to the "Open" JMenuItem
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Open" JMenuItem is selected
+                openNote();  // Calls the method openNote()
             }
         });
-        
-        saveMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                saveNote();
+
+        saveMenuItem.addActionListener(new ActionListener() {  // Adds an action listener to the "Save" JMenuItem
+                public void actionPerformed(ActionEvent e) {  // The method to be called when the "Save" JMenuItem is selected
+                saveNote();  // Calls the method saveNote()
             }
         });
-        
-        saveAsMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                saveNoteAs();
+
+        saveAsMenuItem.addActionListener(new ActionListener() {  // Adds an action listener to the "Save As" JMenuItem
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Save As" JMenuItem is selected
+                saveNoteAs();  // Calls the method saveNoteAs()
             }
         });
-        
-        exitMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                boolean check = promptSaveBeforeExit();
-                if(check){
-                    dispose();
+
+        exitMenuItem.addActionListener(new ActionListener() {  // Adds an action listener to the "Exit" JMenuItem
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Exit" JMenuItem is selected
+                boolean check = promptSaveBeforeExit();  // Calls the method promptSaveBeforeExit() and stores its return value in the variable check
+                if(check){  // If check is true...
+                    dispose();  // ...dispose of the JFrame
                 }
             }
         });
-        
-        newButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                createNewNote();
+
+        newButton.addActionListener(new ActionListener() {  // Adds an action listener to the "New" JButton
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "New" JButton is clicked
+                createNewNote();  // Calls the method createNewNote()
             }
         });
-        
-        openButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                openNote();
+
+        openButton.addActionListener(new ActionListener() {  // Adds an action listener to the "Open" JButton
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Open" JButton is clicked
+                openNote();  // Calls the method openNote()
             }
         });
-        
-        saveButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                saveNote();
+
+        saveButton.addActionListener(new ActionListener() {  // Adds an action listener to the "Save" JButton
+            public void actionPerformed(ActionEvent e) {  // The method to be called when the "Save" JButton is clicked
+                saveNote();  // Calls the method saveNote()
             }
         });
-        
+
+        // the window listener handles window close event
         this.addWindowListener(new WindowAdapter() {
+            // this method is called when the window is about to close
             @Override
             public void windowClosing(WindowEvent e) {
+                // prompt the user to save before exiting
                 boolean check = promptSaveBeforeExit();
+                // if the user wants to save changes or doesn't care about unsaved changes, close the window
                 if(check){
                     dispose();
                 }
             }
         });
+        // make the window visible
         setVisible(true);
     }
     
-    
+     // this method resizes the icon to specified dimensions
     public ImageIcon resizeIcon(String imagePath, int width, int height) {
         ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
         Image img = icon.getImage();
         Image resizedImage = img.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
         return new ImageIcon(resizedImage);
     }
-
+    
+    // this method creates a new note after checking if the current note needs saving
     private void createNewNote() {
         String text = noteTextArea.getText();
         byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
@@ -234,6 +253,7 @@ class VisualNote extends JFrame{
         }
     }
 
+    // this method prompts the user to save changes before exiting
     private boolean promptSaveBeforeExit() {
         String text = noteTextArea.getText();
         byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
@@ -265,7 +285,7 @@ class VisualNote extends JFrame{
         return false;
     }
 
-    
+    // this method opens a note from a file
     private void openNote() {
         JFileChooser fileChooser = new JFileChooser();
         int option = fileChooser.showOpenDialog(null);
@@ -287,6 +307,7 @@ class VisualNote extends JFrame{
         }
     }
 
+    // this method saves the current note to its current file
     private boolean saveNote() {
         if (currentFile != null) {
             try (FileOutputStream fos = new FileOutputStream(currentFile)) {
@@ -302,7 +323,8 @@ class VisualNote extends JFrame{
             return saveNoteAs();
         }
     }
-
+    
+    // this method saves the current note to a new file
     private boolean saveNoteAs() {
         JFileChooser fileChooser = new JFileChooser();
         int option = fileChooser.showSaveDialog(null);
@@ -333,8 +355,11 @@ class VisualNote extends JFrame{
 
 }
 
+// VisualNoteApp class, which is a subclass of VisualNote
 public class VisualNoteApp extends VisualNote {
+    // main method, where the program starts
     public static void main(String[] args) {
+        // create a new VisualNote object, which will display the window
         VisualNote NF = new VisualNote();
     }
 }
